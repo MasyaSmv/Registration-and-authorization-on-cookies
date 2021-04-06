5c9e03a62a7e7db2181673413ad57e7b3cb72e50
Masya Smerdov <61746307+MasyaSmv@users.noreply.github.com>
2021-04-06 11:00:29 +0300

    Проверка добавления

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<link>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MasyaSm</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"></link>
+  <link
+</head>
+<body>
+  <div class="container mt-4">
+    <h1>Тестовый запуск)</h1>
+    <form action="" method="post">
+      <input type="type=text" class="form-control" name="login" id="login" placeholder="Ввеите логин"><br>
+      <input type="type=password" class="form-control" name="pass" id="pass" placeholder="Ввеите пароль"><br>
+      <input type="type=password" class="form-control" name="pass-2" id="pass-2" placeholder="Повторите пароль"><br>
+      <input type="type=text" class="form-control" name="name" id="name" placeholder="Ввеите имя"><br>
+      <input type="type=text" class="form-control" name="mail" id="mail" placeholder="Ввеите почту"><br>
+      <button class="btn btn-success">Зарегистрироваться</button>
+    </form>
+  </div>
+</body>
+</html>
